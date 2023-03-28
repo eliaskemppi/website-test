d7ecd61929d59aac3c3c4a345d73d2c5bb2fd1fd
Elias Kemppi <elias.kemppi@gmail.com>
2023-03-28 16:24:39 +0300

    added frontend

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 app.use(morgan('tiny'))
-
+app.use(express.static('build'))
 
 let persons = [
     { id: 1, name: 'Arto Hellas', number: '040-123456' },
@@ -67,7 +67,7 @@ app.post('/api/persons',  (req, res) => {
     id: id
   }
 
-  
+
   persons = persons.concat(addedPerson)
   res.json(addedPerson)
 })
